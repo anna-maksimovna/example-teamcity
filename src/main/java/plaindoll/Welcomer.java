9971e2e9083a95a4d041786bf8ae4c66157a4cbd
anna.m <anna.m@sunrise-r.ru>
2022-05-23 01:11:24 +0300

    add method

--- src/main/java/plaindoll/Welcomer.java
+++ src/main/java/plaindoll/Welcomer.java
@@ -7,4 +7,7 @@ public class Welcomer{
 	public String sayFarewell() {
 		return "Farewell, good hunter. May you find your worth in waking world.";
 	}
+	public String sayNetologyTest() {
+		return "Netology, good hunter. May you find your worth in waking world.";
+	}
 }
